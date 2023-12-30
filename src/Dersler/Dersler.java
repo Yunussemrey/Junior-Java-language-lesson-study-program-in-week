75b7a0485ee410d4ab66c50424dc65c694598f75
Yunus Emre <yunussemrey1@gmail.com>
2023-12-30 22:16:55 +0300

    paketlerimizi ve içeriğimizi tanımladık.

---
+++ src/Dersler/Dersler.java
@@ -0,0 +1,11 @@
+package Dersler;
+
+public class Dersler {
+    public void liseDersleri(){
+        String ders1="Matematik";
+        String ders2="Kimya";
+        String ders3="Biyoloji";
+        String ders4="İngilizce";
+        String ders5="Almanca";
+    }
+}
---
+++ src/Günler/HaftanınGünleri.java
@@ -0,0 +1,11 @@
+package Günler;
+
+public class HaftanınGünleri {
+    public void dersGünleri(){
+        String gün1="pazartesi";
+        String gün2="Salı";
+        String gün3="Çarşamba";
+        String gün4="Perşembe";
+        String gün5="Cuma";
+    }
+}
