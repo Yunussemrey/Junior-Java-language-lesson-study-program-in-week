3478ebf55e7046116ceb17bf5a6edbe736c09541
Yunus Emre <yunussemrey1@gmail.com>
2023-12-31 14:39:02 +0300

    Junior Java programlama dilinde haftalık ders çalışma programı tamamlandı.

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,19 @@ public class Main {
     public static void main(String[] args) {
 
         //Günleri tanımlayalım;
-        String[] günler={"Pazartesi","Salı","Çarşamba","Perşembe","Cuma"};
-        System.out.println(günler);
+        String[] günler = {"Pazartesi: ", "Salı: ", "Çarşamba: ", "Perşembe: ", "Cuma: "};
+
 
         //Dersleri tanımlayalım;
-        String[] dersler={"Matematik","Kimya","Biyoloji","Fizik","İngilizce"};
+        String[] dersler = {"Matematik /", "Kimya /", "Biyoloji /", "Fizik /", "İngilizce /"};
 
         //Saatleri tanımlayalım;
-        String[] saatler={"9.15-10.15","10.30-11.30","11.45-12.45","13.00-14.00","14.15-15.15"};
-        
+        String[] saatler = {"9.15-10.15", "10.30-11.30", "11.45-12.45", "13.00-14.00", "14.15-15.15"};
+
+        //ders programını çalıştırmak için for döngüsü düzenleyelim;
+        for (int i = 0; i < günler.length; i++) {
+            System.out.println(günler[i] + dersler[i] + saatler[i]);
+        }
 
     }
 }
