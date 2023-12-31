25d521493d25f30fecee86acfdc34f5d4a9a3ef2
Yunus Emre <yunussemrey1@gmail.com>
2023-12-31 14:34:05 +0300

    ekleme silme düzeltme işlemleri yapıldı.

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,16 @@
-import Dersler.Dersler;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-            Dersler ders=new Dersler();
-            ders.liseDersleri();
+        //Günleri tanımlayalım;
+        String[] günler={"Pazartesi","Salı","Çarşamba","Perşembe","Cuma"};
+        System.out.println(günler);
+
+        //Dersleri tanımlayalım;
+        String[] dersler={"Matematik","Kimya","Biyoloji","Fizik","İngilizce"};
+
+        //Saatleri tanımlayalım;
+        String[] saatler={"9.15-10.15","10.30-11.30","11.45-12.45","13.00-14.00","14.15-15.15"};
+        
 
     }
 }
